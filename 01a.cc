9651d1ab8cdf92cd03cb24767ad19cf222114500
Julien Bernard <julien.bernard@univ-fcomte.fr>
2023-11-21 23:34:39 +0100

    add template file

---
+++ 01a.cc
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+
+int main() {
+
+  for (std::string line; std::getline(std::cin, line); ) {
+    if (line.empty() || line == "\n") {
+    }
+  }
+
+}
---
+++ xmake.lua
@@ -0,0 +1,6 @@
+set_project("adventofcode")
+set_version("2023")
+
+target("01a")
+    set_kind("binary")
+    add_files("01a.cc")
